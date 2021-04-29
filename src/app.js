0a2faeb036489169832da0c0c6e4fdec54ed31d4
Denius <denisa5700@yandex.ru>
2021-04-30 08:12:06 +1000

    b1-1

--- src/app.js
+++ src/app.js
@@ -4,17 +4,22 @@ const { config } = require("dotenv");
 const fs = require("fs");
 const path = require("path");
 const term = require("terminal-kit").terminal;
+const { JSDOM } = require("jsdom");
 
 config();
 
-const H_NAME = "saeko_no_shitatari";
-const H_URL = `http://wwv.allhen.live/${H_NAME}/vol1/1`;
+const H_NAME = "lilith_s_code";
+const H_URL = `http://wwv.allhen.live/${H_NAME}/`;
 const SAVE_FOLDER = path.join(__dirname, "saved");
 
 ///rm_h.init(.*)/g
 
 const Main = () => {
 	term(H_URL + "\n");
+
+	ParseTitle(H_URL).then(console.log);
+
+	return;
 	if (fs.existsSync(SAVE_FOLDER))
 		fs.rmdirSync(SAVE_FOLDER, { recursive: true });
 	GetPicturesData().then((pics) => {
@@ -43,27 +48,107 @@ const Main = () => {
 	});
 };
 
+const ParseTitle = (url) => {
+	return new Promise((resolve, reject) => {
+		axios.get(url).then((res) => {
+			const dom = new JSDOM(res.data);
+			const document = dom.window.document;
+			url = new URL(url);
+
+			const surl = new URL(
+				url.protocol +
+					url.hostname +
+					document.querySelector(
+						"a.chapter-link.btn.btn-outline-primary.btn-lg.btn-block.read-first-chapter"
+					).href
+			).href;
+			const info = document.querySelector("div.subject-meta");
+			const toms = info
+				.querySelectorAll("p")[0]
+				.textContent.replace(/\n| |Томов:|Перевод:/g, "");
+			const translate = info
+				.querySelectorAll("p")[1]
+				.textContent.replace(/\n| |Томов:|Перевод:/g, "");
+			const genres = [...info.querySelectorAll(".elem_genre ")].map(
+				(e) => e.querySelector("a").textContent
+			);
+			const authors = [...info.querySelectorAll(".elem_author ")].map(
+				(e) => e.querySelector("a").textContent
+			);
+			const tags = [...info.querySelectorAll(".elem_tag ")].map(
+				(e) => e.querySelector("a").textContent
+			);
+			const categorys = [...info.querySelectorAll(".elem_category ")].map(
+				(e) => e.querySelector("a").textContent
+			);
+			const translators = [
+				...info.querySelectorAll(".elem_translator "),
+			].map((e) => e.querySelector("a").textContent);
+
+			const title = {
+				url: surl,
+				toms,
+				translate,
+				genres,
+				authors,
+				tags,
+				categorys,
+				translators,
+			};
+			resolve(title);
+		});
+	});
+};
+
 const GetPicturesData = () => {
 	return new Promise((resolve, reject) => {
 		axios
 			.get(H_URL)
 			.then((res) => {
-				let data = res.data
-					.match(/rm_h.init(.*)/g)[0]
-					.match(/\[.*\]/g)[0]
-					.replace(/\'/g, '"');
-				data = `[${data}]`;
-				data = JSON.parse(data);
+				const dom = new JSDOM(res.data);
+				const document = dom.window.document;
 
-				let a = data[0];
-				let b = data[1];
+				const urls = [
+					...document
+						.querySelector("select#chapterSelectorSelect")
+						.querySelectorAll("option"),
+				].map((e) => e.value);
 
-				for (b = 0; b < a.length; b++) {
-					var e = a[b];
-					a[b] = { url: "http:" + e[0] + e[2], w: e[3], h: e[4] };
-				}
+				const GetUrl = (epi) => {
+					return urls.find((url) => url.split("/")[3] == epi);
+				};
 
-				resolve(a);
+				const ParsePics = (data) => {
+					data = data
+						.match(/rm_h.init(.*)/g)[0]
+						.match(/\[.*\]/g)[0]
+						.replace(/\'/g, '"');
+					data = `[${data}]`;
+					data = JSON.parse(data);
+
+					let a = data[0];
+					let b = data[1];
+
+					for (b = 0; b < a.length; b++) {
+						var e = a[b];
+						a[b] = { url: "http:" + e[0] + e[2], w: e[3], h: e[4] };
+					}
+
+					return a;
+				};
+
+				const pics = ParsePics(res.data);
+
+				const ExpandPics = () => {
+					while (urls.length - pics.length <= 0) {
+						if (!GetUrl(pics.length - 1)) return;
+						//XXX: axios, parsePics, savePics
+					}
+				};
+
+				ExpandPics();
+
+				resolve(pics);
 			})
 			.catch(reject);
 	});
